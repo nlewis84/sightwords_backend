2f3b7d2cbdc36d2fa13dc1206ee14606beb20870
nllew <n.lewis.perc@gmail.com>
2021-03-14 14:26:12 -0500

    index and create on controllers

--- app/controllers/api/v1/lists_controller.rb
+++ app/controllers/api/v1/lists_controller.rb
@@ -4,4 +4,19 @@ class Api::V1::ListsController < ApplicationController
         render json: ListSerializer.new(List.all)
     end
 
+    def create
+        list = List.new(list_params)
+        if list.save
+            render json: ListSerializer.new(list), status: :accepted
+        else
+            render json: {errors: list.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    private
+
+    def list_params
+        params.require(:list).permit(:name)
+    end
+
 end
--- app/controllers/api/v1/sightwords_controller.rb
+++ app/controllers/api/v1/sightwords_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::SightwordsController < ApplicationController
+
+    def index
+        render json: SightwordSerializer.new(Sightword.all)
+    end
+
+    def create
+        sightword = Sightword.new(sightword_params)
+        if sightword.save
+            render json: SightwordSerializer.new(sightword), status: :accepted
+        else
+            render json: {errors: sightword.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+    end
+
+    private
+
+    def sightword_params
+        params.require(:sightword).permit(:word, :pronunciation_url, :word_url, :list_id)
+    end
+
 end
